817642209379b3d647b412da30bd7fb9a0a45c2f
Sven Speckmaier <sps@ipunkt.biz>
2014-09-25 13:04:39 +0200

    Fixed form type config variable name

--- src/views/user/edit.blade.php
+++ src/views/user/edit.blade.php
@@ -96,8 +96,7 @@
 <div class="row">
     <div class="col-sm-12">
         {{ Form::label( $extra_field['name'], trans($extra_field['name']) ) }}
-        <?php $type = $extra_field['form type'] ?>
-        {{ Form::$type( $extra_field['name'], $user->getExtra($extra_field['name']) ) }}
+        {{ Form::input($extra_field['form_type'], $extra_field['name'], $user->getExtra($extra_field['name']) ) }}
         @if ( $errors->has( $extra_field['name'] ) )
                     <span class="text-danger">
                         {{  trans( 'auth::'. $errors->first( $extra_field['name'] ) )  }}
