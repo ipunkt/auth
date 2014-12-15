92eb4a75d4fab8ee610837db3bc463e464cc22a4
Robert Kummer <github@mail.robert-kummer.de>
2014-12-15 17:46:19 +0100

    fixed typo

--- src/lang/de/user.php
+++ src/lang/de/user.php
@@ -7,28 +7,28 @@
  */
 return [
     'user' => 'Benutzer',
-    'email' => 'Email Addresse',
-    'email_changed' => 'Email Addresse geändert.',
-    'old_password' => 'Momentanes Passwort',
+    'email' => 'E-Mail Adresse',
+    'email_changed' => 'E-Mail Adresse geändert.',
+    'old_password' => 'Bisheriges Passwort',
     'new_password' => 'Neues Passwort',
     'password' => 'Passwort',
     'password_confirmation' => 'Password bestätigen',
     'username' => 'Benutzername',
     'reminder' => [
-        'subject'=> 'Passwort reset anforderung.'
+        'subject'=> 'Passwort wird zurückgesetzt.'
     ],
     'force_reset_set' => 'Sie müssen ihr Passwort ändern bevor sie fortfahren dürfen.',
-    'password_missmatch' => 'Momentanes Passwort inkorrekt.',
+    'password_missmatch' => 'Bisheriges Passwort inkorrekt.',
     'password_changed' => 'Passwort geändert',
     'password_no_confirm' => 'Die eingegebene Passwortbestätigung stimmt nicht mit dem Passwort überein.',
-    'password_old' => 'Das neue Passwort stimmt mit dem momentan gesetzten überein.',
+    'password_old' => 'Das neue Passwort stimmt mit dem bisher gesetzten überein.',
     'permission_failed' => 'Keine Berechtigung um :action auf :object auszuführen.',
     'enabled' => 'Freigeschaltet',
-    'enabled_changed' => 'Freigeschaltung geändert',
-    'edit permission' => 'Editieren',
+    'enabled_changed' => 'Freischaltung geändert',
+    'edit permission' => 'Bearbeiten',
     'delete success' => 'Benutzer :username erfolgreich gelöscht',
     'delete permission denied' => 'Berechtigung fehlt um :username löschen zu dürfen',
-    'logout success' => 'Benutzer :user erfolgreich ausgeloggt.',
+    'logout success' => 'Benutzer :user erfolgreich abgemeldet.',
 
-    'register success' => 'Benutzer :user erfolgreich registriert',
+    'register success' => 'Benutzer :user erfolgreich registriert.',
 ];
