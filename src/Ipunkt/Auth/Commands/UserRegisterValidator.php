243330f61fe5efdd7204829149745ba09fa789bb
Robert Kummer <rok@ipunkt.biz>
2015-02-06 16:21:58 +0100

    added unique validator to user register command validation

--- src/Ipunkt/Auth/Commands/UserRegisterValidator.php
+++ src/Ipunkt/Auth/Commands/UserRegisterValidator.php
@@ -4,13 +4,21 @@ use Illuminate\Config\Repository;
 use Ipunkt\Auth\Exceptions\ValidationFailedException;
 use Illuminate\Validation\Factory as Validator;
 
-class UserRegisterValidator {
-	
+class UserRegisterValidator
+{
+	/**
+	 * validation ruleset
+	 *
+	 * @var array
+	 */
 	private $rules = [
 		'email' => 'required|email',
 		'password' => 'required|min:1',
 	];
+
 	/**
+	 * validator factory
+	 *
 	 * @var Validator
 	 */
 	private $validator;
@@ -19,27 +27,46 @@ class UserRegisterValidator {
 	 * @param Repository $config
 	 * @param Validator $validator
 	 */
-	public function __construct(Repository $config, Validator $validator) {
-	    $extraFields = $config->get('auth::user_table.extra_fields');
-		foreach($extraFields as $extraField) {
+	public function __construct(Repository $config, Validator $validator)
+	{
+		$extraFields = $config->get('auth::user_table.extra_fields');
+		foreach ($extraFields as $extraField) {
 			$this->rules[$extraField['name']] = $extraField['validation_rules'];
 		}
 		$this->validator = $validator;
+
+		$this->setupRules($config);
 	}
 
 	/**
 	 * @param UserRegisterCommand $command
+	 *
 	 * @return bool
 	 */
-	public function validate(UserRegisterCommand $command) {
+	public function validate(UserRegisterCommand $command)
+	{
 		/**
 		 * @var Validator $validator
 		 */
 		$validator = $this->validator->make($command->toArray(), $this->rules);
-		
-		if( $validator->fails() )
+
+		if ($validator->fails())
 			throw new ValidationFailedException($validator->errors());
-		
+
 		return true;
+	}
+
+	/**
+	 * setting up the rules with additional unique field validation
+	 *
+	 * @param \Illuminate\Config\Repository $config
+	 */
+	private function setupRules(Repository $config)
+	{
+		//  @TODO extra_fields in rules aufnehmen (Stichwort: unique)
+
+		$tableName = $config->get('auth::user_table.table_name');
+
+		$this->rules['email'] .= '|unique:' . $tableName . ',email';
 	}
 }
